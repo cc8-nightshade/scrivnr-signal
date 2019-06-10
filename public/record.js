54ba7893dbbe40444b776cdb08e8bd65d198ea58
Ian Cameron <ian.kameron@gmail.com>
2019-06-10 10:58:34 +0900

    added recording, sample transcription data

--- public/record.js
+++ public/record.js
@@ -2,14 +2,15 @@ if (navigator.mediaDevices) {
   console.log('getUserMedia supported.');
 
   // var myConstraints = { audio: true };
-  var myConstraints = {audio: {
-    sampleRate: 16000,
-    channelCount: 1,
-    volume: 1.0,
-    echoCancellation: true
+  var myConstraints = {audio: true
+    // {
+    // sampleRate: 16000,
+    // channelCount: 1,
+    // volume: 1.0,
+    // echoCancellation: true
     // ,
     // noiseSuppression: true,
-  }};
+  };
 
   var chunks = [];
 
@@ -17,25 +18,40 @@ if (navigator.mediaDevices) {
   let stop = document.getElementById("stop-button");
   let soundClips = document.getElementById("audio-player");
 
+  let reader = new FileReader();
+
   navigator.mediaDevices.getUserMedia(myConstraints)
   .then(function(stream) {
     console.log("inside the then statement after mounting media")
-    var mediaRecorder = new MediaRecorder(stream);
-
+    var mediaRecorder = new MediaStreamRecorder(stream);
+    mediaRecorder.mimeType = 'audio/wav'; // check this line for audio/wav
+    // mediaRecorder.mimeType = 'audio/ogg'; // check this line for audio/wav
+    mediaRecorder.ondataavailable = async (blob) => {
+      chunks.push(blob);
+      console.log("pushing blob", chunks);
+      
+      reader.readAsDataURL(blob); 
+      reader.onloadend = function() {
+        mySocket.emit("send-blob", reader.result.substring(22));
+        console.log(reader.result.substring(22));
+      }
+      // mySocket.emit("send-blob", blob.toString("base64"));
+      // console.log("base64 blob", blob.toString("base64"))
+    };
     //visualize(stream);
 
     record.onclick = function() {
-      mediaRecorder.start(1000);
-      console.log(mediaRecorder.state);
+      mediaRecorder.start();
+      //console.log(mediaRecorder.state);
       console.log("recorder started");
       record.style.background = "red";
       record.style.color = "black";
-      console.log(mediaRecorder.stream);
+      //console.log(mediaRecorder.stream);
     }
 
     stop.onclick = function() {
       mediaRecorder.stop();
-      console.log(mediaRecorder.state);
+      console.log("mediarecorder state", mediaRecorder.state);
       console.log("recorder stopped");
       record.style.background = "";
       record.style.color = "";
@@ -63,11 +79,13 @@ if (navigator.mediaDevices) {
 
       audio.controls = true;
       // var blob = new Blob(chunks, { 'type' : 'audio/wav' });
-      var blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+      var blob = new Blob(chunks, { 'type' : 'audio/wav' });
+      console.log(chunks[0].size, blob.size)
       chunks = [];
       var audioURL = URL.createObjectURL(blob);
       audio.src = audioURL;
-      console.log("recorder stopped");
+      console.log("recorder stopped", );
+      mySocket.emit("end-record");
 
       deleteButton.onclick = function(e) {
         evtTgt = e.target;
@@ -75,12 +93,80 @@ if (navigator.mediaDevices) {
       }
     }
 
-    mediaRecorder.ondataavailable = function(e) {
-      chunks.push(e.data);
-    }
+    // mediaRecorder.ondataavailable = function(e) {
+    //   chunks.push(e.data);
+    // }
   })
   .catch(function(err) {
     console.log('The following error occurred: ' + err);
-  })
+  });
 }
+
+  // OLD MEDIARECORDER METHODS
+//   navigator.mediaDevices.getUserMedia(myConstraints)
+//   .then(function(stream) {
+//     console.log("inside the then statement after mounting media")
+//     var mediaRecorder = new MediaRecorder(stream);
+
+//     //visualize(stream);
+
+//     record.onclick = function() {
+//       mediaRecorder.start(1000);
+//       console.log(mediaRecorder.state);
+//       console.log("recorder started");
+//       record.style.background = "red";
+//       record.style.color = "black";
+//       console.log(mediaRecorder.stream);
+//     }
+
+//     stop.onclick = function() {
+//       mediaRecorder.stop();
+//       console.log(mediaRecorder.state);
+//       console.log("recorder stopped");
+//       record.style.background = "";
+//       record.style.color = "";
+//     }
+
+//     mediaRecorder.onstop = function(e) {
+//       console.log("data available after MediaRecorder.stop() called.");
+
+//       var clipName = prompt('Enter a name for your sound clip');
+
+//       var clipContainer = document.createElement('article');
+//       var clipLabel = document.createElement('p');
+//       var audio = document.createElement('audio');
+//       var deleteButton = document.createElement('button');
+     
+//       clipContainer.classList.add('clip');
+//       audio.setAttribute('controls', '');
+//       deleteButton.innerHTML = "Delete";
+//       clipLabel.innerHTML = clipName;
+
+//       clipContainer.appendChild(audio);
+//       clipContainer.appendChild(clipLabel);
+//       clipContainer.appendChild(deleteButton);
+//       soundClips.appendChild(clipContainer);
+
+//       audio.controls = true;
+//       // var blob = new Blob(chunks, { 'type' : 'audio/wav' });
+//       var blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+//       chunks = [];
+//       var audioURL = URL.createObjectURL(blob);
+//       audio.src = audioURL;
+//       console.log("recorder stopped");
+
+//       deleteButton.onclick = function(e) {
+//         evtTgt = e.target;
+//         evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
+//       }
+//     }
+
+//     mediaRecorder.ondataavailable = function(e) {
+  //       chunks.push(e.data);
+  //     }
+//   })
+//   .catch(function(err) {
+//     console.log('The following error occurred: ' + err);
+//   })
+// }
 
--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const PORT = process.env.PORT || 9000;
 const fs = require("fs");
 var https = require('https');
+const speech = require('@google-cloud/speech');
+
 
 var privateKey = fs.readFileSync('./server/ssl/server.key');
 var certificate = fs.readFileSync('./server/ssl/server.cert');
@@ -37,8 +39,14 @@ httpsServer.listen(9000);
 let io = socket(httpsServer);
 
 let connectedUsers = [];
+let blobData = [];
 
 io.on("connection", (socket) => {
+  socket.on("send-blob", (blob64) => {
+    console.log(blob64.length);
+    blobData.push(Buffer.from(blob64, "base64"));
+    console.log(blobData);
+  });
   socket.on("initialize", () => {
     console.log("receiving initialization from", socket.id);
     //io.to(socket.id).emit("message", "You are connected");
@@ -92,5 +100,68 @@ io.on("connection", (socket) => {
     }
     io.to(targetUser).emit("hang-up");
   });
+  socket.on("end-record", () => {
+    // console.log(typeof blobData[0]);
+    
+    var allAudio = Buffer.concat(blobData);
+    
+    // getTranscription(allAudio.toString("base64")).catch(console.error);;
+    // for (let i = 1; i < blobData.length; i++) {
 
+    //   console.log(blobData[i].toString("base64"));
+    // }
+    getTranscription(allAudio.toString("base64")).catch(console.error);;
+    //fs.writeFileSync("./server/wavtest.wav", )
+  });
 });
+
+async function getTranscription(audioBytes) {
+  // Creates a client
+  const client = new speech.SpeechClient();
+ 
+  // // The name of the audio file to transcribe
+  // const fileName = './server/speech_16kbps_wb.wav';
+
+  // Reads a local audio file and converts it to base64
+  // const file = fs.readFileSync(fileName);
+  // const audioBytes = file.toString('base64');
+  // console.log(audioBytes);
+ 
+  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+  
+  console.log("length of thing being sent to google", audioBytes.length);
+  const audio = {
+    content: audioBytes,
+    // data: file,
+  };
+  const config = {
+    // encoding: 'AMR',
+    encoding: 'LINEAR16',
+    // encoding: 'OGG_OPUS',
+    // sampleRateHertz: 16000,
+    languageCode: 'en-US',
+    audioChannelCount: 2,
+    enableSeparateRecognitionPerChannel: false,
+    enableAutomaticPunctuation: true,
+    enableWordTimeOffsets: true,
+  };
+  const request = {
+    audio: audio,
+    config: config,
+  };
+ 
+  // Detects speech in the audio file
+  const [operation] = await client.longRunningRecognize(request);
+  // const response = await operation.promise();
+  const fullResponse = await operation.promise();
+  const [response] = fullResponse;
+  console.log(response);
+  // const [response] = await client.recognize(request);
+  // console.log(response);
+  fs.writeFileSync("./server/sample-transcription.json", JSON.stringify(fullResponse));
+  console.log(JSON.stringify(response));
+  // const transcription = response.results
+  //   .map(result => result.alternatives[0].transcript)
+  //   .join('\n');
+  // console.log(`Transcription: ${transcription}`);
+}
